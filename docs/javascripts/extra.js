b63eea68aa1e2421f931847930ddba06a291ff07
ScottCheung930 <yuqizhan@kth.se>
2025-02-23 15:52:32 +0100

    Some changes

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -21,29 +21,6 @@ window.addEventListener('load', function () {
     header.addEventListener('dblclick', () => {
         header.classList.toggle('expanded');
     });
-
-    // For mobile: detect drag down gesture
-    header.addEventListener('touchstart', (e) => {
-        if (e.touches.length === 1) {
-            startY = e.touches[0].clientY;
-        }
-    });
-
-    header.addEventListener('touchmove', (e) => {
-        if (startY !== null) {
-            const currentY = e.touches[0].clientY;
-            // If the user has dragged down more than 50px, expand the header
-            if (currentY - startY > 50) {
-                header.classList.add('expanded');
-            }
-        }
-    });
-
-    header.addEventListener('touchend', () => {
-        // Reset the starting touch position.
-        startY = null;
-        header.classList.remove('expanded');
-    });
 });
 
 
